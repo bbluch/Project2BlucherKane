de5a7e04c46a096b040f6d0f494b8bb579a80fe0
bbluch <benblucher@vt.edu>
2025-10-07 16:04:18 -0400

    test to add a class
    Added class for BSTNode

---
+++ src/BSTNode.java
@@ -0,0 +1,19 @@
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ * 
+ *  @author bluch
+ *  @version Oct 7, 2025
+ * @param <K> 
+ * @param <V> 
+ */
+public class BSTNode<K, V> {
+    //~ Fields ................................................................
+
+    //~ Constructors ..........................................................
+
+    //~Public  Methods ........................................................
+
+}
