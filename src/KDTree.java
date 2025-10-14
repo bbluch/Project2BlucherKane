cdc5a854f1351c2c7ad397b1224b338172b6487b
austink23 <ajkanevt27@gmail.com>
2025-10-14 15:38:12 -0400

    deleted comments

--- src/KDTree.java
+++ src/KDTree.java
@@ -43,23 +43,23 @@ public class KDTree {
         setNodeCount(size() + 1);
     }
 
-// /**
-// * Removes a city from the KD Tree.
-// *
-// * @param x
-// * The x-coordinate of the city to remove.
-// * @param y
-// * The y-coordinate of the city to remove.
-// * @return The removed city, or null if not found.
-// */
-// public City remove(int x, int y) {
-// City temp = find(x, y);
-// if (temp != null) {
-// root = removeHelp(root, x, y, 0);
-// setNodeCount(size() - 1);
-// }
-// return temp;
-// }
+     /**
+     * Removes a city from the KD Tree.
+     *
+     * @param x
+     * The x-coordinate of the city to remove.
+     * @param y
+     * The y-coordinate of the city to remove.
+     * @return The removed city, or null if not found.
+     */
+     public City remove(int x, int y) {
+         City temp = find(x, y);
+         if (temp != null) {
+             root = removeHelp(root, x, y, 0);
+             setNodeCount(size() - 1);
+         }
+         return temp;
+     }
 
 
     /**
@@ -178,121 +178,120 @@ public class KDTree {
         return findHelp(rt.getRight(), x, y, level + 1);
     }
 
-// /**
-// * Helper method for remove logic.
-// *
-// * @param rt
-// * root node
-// * @param x
-// * The x-coordinate for the city
-// * @param y
-// * The y-coordnate for the city
-// * @param level
-// * The level of the tree
-// * @return node being removed, null if not found
-// */
-// private BSTNode removeHelp(BSTNode rt, int x, int y, int level) {
-// if (rt == null) {
-// return null;
-// }
-//
-// int axis = level % 2;
-// int compare;
-//
-// if (rt.getCity().getX() == x && rt.getCity().getY() == y) {
-// if (rt.getRight() != null) {
-// City min = findMin(rt.getRight(), axis, level + 1);
-// rt.setCity(min);
-// rt.setRight(removeHelp(rt.getRight(), min.getX(), min.getY(),
-// level + 1));
-// }
-// else if (rt.getLeft() != null) {
-// City min = findMin(rt.getLeft(), axis, level + 1);
-// rt.setCity(min);
-// rt.setRight(removeHelp(rt.getLeft(), min.getX(), min.getY(),
-// level + 1));
-// rt.setLeft(null); // The left subtree is now empty
-// }
-// else { // Leaf node
-// return null;
-// }
-// return rt;
-// }
-//
-// if (axis == 0) {
-// compare = x - rt.getCity().getX();
-// }
-// else {
-// compare = y - rt.getCity().getY();
-// }
-//
-// if (compare < 0) {
-// rt.setLeft(removeHelp(rt.getLeft(), x, y, level + 1));
-// }
-// else {
-// rt.setRight(removeHelp(rt.getRight(), x, y, level + 1));
-// }
-//
-// return rt;
-// }
-
-// /**
-// * Finds the node with minimum value.
-// *
-// * @param rt
-// * root node
-// * @param axis
-// * The determining count for comparison
-// * @param level
-// * The level of the tree
-// * @return The city with minimum value
-// */
-// private City findMin(BSTNode rt, int axis, int level) {
-// if (rt == null) {
-// return null;
-// }
-//
-// int currentAxis = level % 2;
-//
-// if (currentAxis == axis) {
-// if (rt.getLeft() == null) {
-// return rt.getCity();
-// }
-// return findMin(rt.getLeft(), axis, level + 1);
-// }
-// City leftMin = findMin(rt.getLeft(), axis, level + 1);
-// City rightMin = findMin(rt.getRight(), axis, level + 1);
-// City rootCity = rt.getCity();
-//
-// City min = rootCity;
-// if (leftMin != null && compareCities(leftMin, min, axis) < 0) {
-// min = leftMin;
-// }
-// if (rightMin != null && compareCities(rightMin, min, axis) < 0) {
-// min = rightMin;
-// }
-// return min;
-// }
-
-// /**
-// * Compares two cities, returning based on KDTree logic.
-// *
-// * @param c1
-// * First city being compared.
-// * @param c2
-// * Second city being compared.
-// * @param axis
-// * The determining comparison count.
-// * @return result of comparison
-// */
-// private int compareCities(City c1, City c2, int axis) {
-// if (axis == 0) {
-// return Integer.compare(c1.getX(), c2.getX());
-// }
-//
-// return Integer.compare(c1.getY(), c2.getY());
-//
-// }
+     /**
+     * Helper method for remove logic.
+     *
+     * @param rt
+     *  root node
+     * @param x
+     *  The x-coordinate for the city
+     * @param y
+     *  The y-coordnate for the city
+     * @param level
+     *  The level of the tree
+     * @return node being removed, null if not found
+     */
+     private BSTNode removeHelp(BSTNode rt, int x, int y, int level) {
+         if (rt == null) {
+             return null;
+         }
+         int axis = level % 2;
+         int compare;
+    
+         if (rt.getCity().getX() == x && rt.getCity().getY() == y) {
+             if (rt.getRight() != null) {
+                 City min = findMin(rt.getRight(), axis, level + 1);
+                 rt.setCity(min);
+                 rt.setRight(removeHelp(rt.getRight(), min.getX(), min.getY(),
+                     level + 1));
+             }
+             else if (rt.getLeft() != null) {
+                 City min = findMin(rt.getLeft(), axis, level + 1);
+                 rt.setCity(min);
+                 rt.setRight(removeHelp(rt.getLeft(), min.getX(), min.getY(),
+                     level + 1));
+                 rt.setLeft(null); // The left subtree is now empty
+             }
+             else { // Leaf node
+                 return null;
+             }
+             return rt;
+         }
+    
+         if (axis == 0) {
+             compare = x - rt.getCity().getX();
+         }
+         else {
+             compare = y - rt.getCity().getY();
+         }
+    
+         if (compare < 0) {
+             rt.setLeft(removeHelp(rt.getLeft(), x, y, level + 1));
+         }
+         else {
+             rt.setRight(removeHelp(rt.getRight(), x, y, level + 1));
+         }
+    
+         return rt;
+         }
+    
+     /**
+     * Finds the node with minimum value.
+     *
+     * @param rt
+     * root node
+     * @param axis
+     * The determining count for comparison
+     * @param level
+     * The level of the tree
+     * @return The city with minimum value
+     */
+     private City findMin(BSTNode rt, int axis, int level) {
+         if (rt == null) {
+             return null;
+         }
+    
+         int currentAxis = level % 2;
+    
+         if (currentAxis == axis) {
+             if (rt.getLeft() == null) {
+                 return rt.getCity();
+             }
+             return findMin(rt.getLeft(), axis, level + 1);
+         }
+         City leftMin = findMin(rt.getLeft(), axis, level + 1);
+         City rightMin = findMin(rt.getRight(), axis, level + 1);
+         City rootCity = rt.getCity();
+    
+         City min = rootCity;
+         if (leftMin != null && compareCities(leftMin, min, axis) < 0) {
+             min = leftMin;
+         }
+         if (rightMin != null && compareCities(rightMin, min, axis) < 0) {
+             min = rightMin;
+         }
+         return min;
+     }
+    
+    /**
+     * Compares two cities, returning based on KDTree logic.
+     *
+     * @param c1
+     * First city being compared.
+     * @param c2
+     * Second city being compared.
+     * @param axis
+     * The determining comparison count.
+     * @return result of comparison
+     */
+     private int compareCities(City c1, City c2, int axis) {
+     if (axis == 0) {
+     return Integer.compare(c1.getX(), c2.getX());
+     }
+    
+     return Integer.compare(c1.getY(), c2.getY());
+    
+     }
 
 
     /**
@@ -343,23 +342,6 @@ public class KDTree {
             pointValue = y;
         }
 
-        // Recursively search the appropriate subtrees
-//        if (pointValue < axisValue) {
-//            count += regionSearchHelp(rt.getLeft(), x, y, radius, level + 1,
-//                results);
-//            if (Math.abs(axisValue - pointValue) < radius) {
-//                count += regionSearchHelp(rt.getRight(), x, y, radius, level
-//                    + 1, results);
-//            }
-//        }
-//        else {
-//            count += regionSearchHelp(rt.getRight(), x, y, radius, level + 1,
-//                results);
-//            if (Math.abs(axisValue - pointValue) < radius) {
-//                count += regionSearchHelp(rt.getLeft(), x, y, radius, level + 1,
-//                    results);
-//            }
-//        }
         
         if (pointValue - radius < axisValue) {
             count += regionSearchHelp(rt.getLeft(), x, y, radius, level + 1,
