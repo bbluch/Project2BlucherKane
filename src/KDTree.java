7139a01a10a0cb6dcfd868d3e63810706832fb9f
bbluch <benblucher@vt.edu>
2025-10-14 20:28:36 -0400

    Update KDTree.java

--- src/KDTree.java
+++ src/KDTree.java
@@ -375,6 +375,7 @@ public class KDTree {
             return null;
         result.nodesVisited++; // Count node visit in findMin as well
 
+        //int axis = level % 2;
         BSTNode leftMin = findMinNode(rt.getLeft(), axis, level + 1, result);
         BSTNode rightMin = findMinNode(rt.getRight(), axis, level + 1, result);
         BSTNode min = rt;
